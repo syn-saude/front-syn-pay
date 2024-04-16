5e7371705001514ad106d6539482218940df8157
Roger Rodrigues Costa <rrodrigues@outlook.com>
2024-04-16 10:43:24 -0300

    Schema validacao add financiamento

--- app/financiamentos/add/page.tsx
+++ app/financiamentos/add/page.tsx
@@ -112,6 +112,7 @@ const etapas = [
 //#endregion
 function Add() {
   const [currentStep, setCurrentStep] = useState(1)
+
   const { register, watch, handleSubmit, setValue, formState, control } =
     useForm<FinanciamentoRequest>({
       resolver: yupResolver(schema),
@@ -132,7 +133,7 @@ function Add() {
     // register("inputCurrency")
   }, [register])
   return (
-    <div className=" flex min-h-screen w-full flex-col bg-muted/40">
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <div className="container flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
           <Sheet>
@@ -222,7 +223,7 @@ function Add() {
 
               <DevTool control={control} />
             </nav>
-            <div className="grid gap-6">
+            <div className="grid gap-6 ">
               <Card x-chunk="dashboard-04-chunk-1">
                 <CardHeader>
                   <CardTitle>Step {currentStep}</CardTitle>
